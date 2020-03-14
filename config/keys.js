597fa86ab0cece1eb72385a3b39c0f0d9cf952a7
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-03-14 13:44:25 -0500

    adding config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/mern-auth", 
+  mongoURI: "mongodb://heroku_r0hz2b6b:jvibha3k3vao9b6lhq2tolvas1@ds017886.mlab.com:17886/heroku_r0hz2b6b", 
   secretOrKey: "secret"
   };
