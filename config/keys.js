46903617589c412854d3d6795f55600cd9a265e6
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-03-19 18:09:49 -0500

    last min fix

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://heroku_r0hz2b6b:jvibha3k3vao9b6lhq2tolvas1@ds017886.mlab.com:17886/heroku_r0hz2b6b", 
+  mongoURI: process.env.MONGODB_URI || "mongodb://localhost/store",
   secretOrKey: "secret"
   };
