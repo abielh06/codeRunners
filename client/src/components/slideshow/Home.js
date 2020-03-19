19d4e6855e7a76664edf6763d61a3cdb1722892a
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-03-19 01:09:23 -0500

    adding images to home

--- client/src/components/slideshow/Home.js
+++ client/src/components/slideshow/Home.js
@@ -6,20 +6,17 @@ import image1 from './utsa.jpg'
 import image2 from './utsa2.jpeg'
 import image3 from './utsa3.jpg'
 import image4 from './utsa4.jpg'
-import image5 from './utsa5.jpg'
 import image6 from './utsa6.jpg'
 import image7 from './utsa7.jpg'
-import image8 from './utsa8.jpg'
-import image9 from './utsa9.jpg'
-import image11 from './utsa11.jpg'
-import image12 from './utsa12.jpg'
+import image13 from './utsa13.jpg'
+import image14 from './utsa14.jpg'
 
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <BackgroundSlideshow images={[ image1, image2, image3, image4, image5, image6, image7, image8, image9, image11, image12 ]} />
+        <BackgroundSlideshow images={[ image1, image2, image3, image4, image6, image7, image13, image14 ]} />
       </div>
     )
   }
